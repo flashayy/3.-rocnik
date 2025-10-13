fd9d64d0648334be65802894b0d57e221e70c0b9
flashayy <89212162+flashayy@users.noreply.github.com>
2025-10-13 11:46:04 +0200

    Vytvoril som tabulku

---
+++ DGT/Dynamické zobrazenie úloh/main.py
@@ -0,0 +1,25 @@
+from browser import document, html
+
+tasks = [   
+    {"title": "Finish Homework", "done": False, "priority": "low"},      
+    {"title": "Feed the dog", "done": True, "priority": "mid"},    
+    {"title": "Take the trash out", "done": False, "priority": "high"}   
+] 
+
+def list_tasks():
+    my_div = document["tasks"]
+    my_div.clear()
+    tab = html.TABLE(Class = "task.table")
+    header = html.TR([html.TH("Task"), html.TH("Finished"), html.TH("Priority")])
+    tab <= header
+    
+    for t in tasks:
+        row = html.TR()
+        row <= html.TD(t["title"])
+        row <= html.TD("OK" if t["done"] else "TBD")
+        row <= html.TD(t["priority"])
+        tab <= row
+        
+    my_div <= tab
+    
+list_tasks
