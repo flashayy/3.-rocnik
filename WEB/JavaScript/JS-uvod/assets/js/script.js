fb048f7ee6222b9f703de9772667f7342f56b98a
flashayy <89212162+flashayy@users.noreply.github.com>
2025-11-18 09:21:46 +0100

    pridal som premennu text

--- WEB/JavaScript/JS-uvod/assets/js/script.js
+++ WEB/JavaScript/JS-uvod/assets/js/script.js
@@ -2,9 +2,11 @@ console.log("JS funguje")
 
 const btn = document.getElementById("btn")
 
+const text = document.getElementById("text")
 
 if (btn){
     btn.addEventListener("click", () => {
 
     })
 }
+
