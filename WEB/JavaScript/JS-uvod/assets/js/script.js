6ab92dd1985e4d4b6b450429a1ac25f80cdc481b
flashayy <89212162+flashayy@users.noreply.github.com>
2025-11-18 09:25:43 +0100

    tlacidlo funguje

--- WEB/JavaScript/JS-uvod/assets/js/script.js
+++ WEB/JavaScript/JS-uvod/assets/js/script.js
@@ -4,9 +4,9 @@ const btn = document.getElementById("btn")
 
 const text = document.getElementById("text")
 
-if (btn){
+if (btn && text){
     btn.addEventListener("click", () => {
-
+        text.textContent = "Novy obsah, ktory som zmenil cez JS";
     })
 }
 
