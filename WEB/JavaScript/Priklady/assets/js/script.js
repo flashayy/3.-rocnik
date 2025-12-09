0f843f4fab4fed61e9b6a0495a5c05224995e0b0
flashayy <89212162+flashayy@users.noreply.github.com>
2025-12-09 09:29:10 +0100

    Porovnavanie cisiel

---
+++ WEB/JavaScript/Priklady/assets/js/script.js
@@ -0,0 +1,11 @@
+let number = prompt("Zadaj cislo.")
+
+if (number > 0) {
+    console.log("Tvoje cislo je kladne")
+}
+else if (number < 0) {
+    console.log("Tvoje cislo je zaporne")
+}
+else if (number == 0) {  
+    console.log("Tvoje cislo je nula")
+}
