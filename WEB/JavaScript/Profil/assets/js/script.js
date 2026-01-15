eefb1310ea83b7883371e53d309825d7eeacdcf8
flashayy <samuel.michalik08@gmail.com>
2026-01-15 13:30:45 +0100

    idk

--- WEB/JavaScript/Profil/assets/js/script.js
+++ WEB/JavaScript/Profil/assets/js/script.js
@@ -12,3 +12,22 @@ const konicek = "dizajn";
 
 // VYPOCTY
 
+const rokNarodenia = 2025 - vek
+const doDospelosti = 18 - vek
+
+// ZOBRAZENIE UDAJOV V HTML
+
+document.getElementById("meno").textContent = meno;
+document.getElementById("vek").textContent = vek;
+document.getElementById("mesto").textContent = mesto;
+document.getElementById("konicek").textContent = konicek;
+document.getElementById("rok-narodenia").textContent = rokNarodenia;
+document.getElementById("do-dospelosti").textContent = doDospelosti;
+
+// BIO
+
+const text = 'Ahoj! Volám sa ${meno}, mám ${vek}, rokov a pochadzam z ${mesto}. Vo volnom case rad robim ${konicek}.';
+document.getElementsById("o-mne").textContent = text;
+
+// AKTUALIZOVANIE STATUSU
+document.getElementById("status").textContent = "Profil načítaný!";
