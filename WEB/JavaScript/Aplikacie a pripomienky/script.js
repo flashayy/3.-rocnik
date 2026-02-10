e40bd3fefa3c9324be53d0b71a27da0680ee3151
flashayy <89212162+flashayy@users.noreply.github.com>
2026-02-10 09:20:55 +0100

    Dokoncil som ulohu

--- WEB/JavaScript/Aplikacie a pripomienky/script.js
+++ WEB/JavaScript/Aplikacie a pripomienky/script.js
@@ -12,8 +12,37 @@ const taskList = document.getElementById('taskList');
 
 addBtn.addEventListener('click', () => {
     const taskText = taskInput.value.trim();
+    if (!taskText) return;
+
+    let listItem = document.createElement('li');
+    let listInput = document.createElement('input');
+    let listSpan = document.createElement('span');
+    let listBtn = document.createElement('button');
+
+    listItem.className = 'task-item';
+
+    listInput.type = 'checkbox';
+    listInput.className = 'task-checkbox';
+
+    listSpan.className = 'task-text';
+    listSpan.textContent = taskText;
+
+    listBtn.className = 'delete-button';
+    listBtn.textContent = 'Zmazať';
+
+    listInput.addEventListener('change', () => {
+        listItem.classList.toggle('completed');
+    });
+
+    listBtn.addEventListener('click', () => {
+        listItem.remove();
+    });
+
+    listItem.appendChild(listInput);
+    listItem.appendChild(listSpan);
+    listItem.appendChild(listBtn);
+
+    taskList.appendChild(listItem);
+
+    taskInput.value = '';
 });
-
-if (!taskText) return;
-
-let listItem = document.createElement('li');
