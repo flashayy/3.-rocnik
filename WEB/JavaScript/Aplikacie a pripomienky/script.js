ccf07d5cbe82e759004758c1da5ec2dcf5f94f48
flashayy <89212162+flashayy@users.noreply.github.com>
2026-02-10 08:32:33 +0100

    Pridal som event listener na addBtn ziskal hodnotu textu a ak v texte nic neni napisane sa nic nestane

---
+++ WEB/JavaScript/Aplikacie a pripomienky/script.js
@@ -0,0 +1,19 @@
+//=================================================
+// Premenne
+//==============================================
+
+const taskInput = document.getElementById('taskInput');
+const addBtn = document.getElementById('addBtn');
+const taskList = document.getElementById('taskList');
+
+//=================================================
+// Funkcie
+//=================================================
+
+addBtn.addEventListener('click', () => {
+    const taskText = taskInput.value.trim();
+});
+
+if (!taskText) return;
+
+
